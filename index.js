9e4cb79f845b6fb37f0cb414b3697da64a14c699
shyamshilu <shyamshilu77@gmail.com>
2025-07-15 11:04:31 +0530

    5 program

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-const a="shyam"
-console.log(`hello ${a}`)
-console.log("hello "+a)
+
+// git clone link
+
+// file upload
+// git add .
+// git commit -m "message"
+// git push
+
+// git pull
+
+// const a="shyam"
+// console.log(`hello ${a}`)
+// console.log("hello "+a)
+
+// 1 check even or odd
+
+// const n=12
+// if(n%2==0)
+// {
+//     console.log("Even")
+// }else{
+// console.log("odd")
+// }
+
+//2 find maximum of two numbers
+// const n=12
+// const p=10
+// if(n>p)
+// {
+//     console.log("N is big")
+// }else{
+//     console.log("p is big")
+// }
+
+//3 check leap year
+// const year=2025
+// if(year%4==0)
+// {
+//     console.log("leap year")
+// }else{
+//     console.log("not leap year")
+// }
+
+//4 sum of natural number
+// const n=5
+// let sum=0
+// for(i=1;i<=n;i++)
+// {
+//     sum+=i
+// }  
+// console.log(sum)
+
+//5 factorial of number
+// const n=5
+// let fact=1
+// for(i=1;i<=n;i++)
+// {
+//     fact*=i
+// }  
+// console.log(fact)
