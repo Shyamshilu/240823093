e9fdfb22ef3caaabb400b1d7916fc193cc4a1ef8
shyamshilu <shyamshilu77@gmail.com>
2025-07-22 10:32:20 +0530

    package

--- index.js
+++ index.js
@@ -69,3 +69,91 @@
 // }
 
 //7 reverse number
+//  function reverseNumber(num)
+//   {
+//     return parseInt(String(num).split('').reverse().join(''));
+//   }
+//     console.log(`Input: 1234, Output: ${reverseNumber(1234)}`);     
+
+//8 palindrom
+  
+// function isPalindrome(num) {
+//     if (num < 0) {
+//       return false;
+//     }
+//     const numStr = String(num);
+//     const reversedStr = numStr.split('').reverse().join('');  
+//       return numStr === reversedStr;
+//   }  
+//   const num1 = 121;
+//   console.log(`Input: ${num1}, Output: ${isPalindrome(num1) ? 'Palindrome' : 'Not Palindrome'}`); 
+  
+//   const num3 = 12345;
+//   console.log(`Input: ${num3}, Output: ${isPalindrome(num3) ? 'Palindrome' : 'Not Palindrome'}`); 
+
+
+//9 check prime number
+
+// function isprime(n)
+// {
+//     if(n<=1) return "not prime";
+//     for(let i=2;i<=Math.sqrt(n);i++)
+//     {
+//         if(n % i == 0) return " not prime"
+//     }
+//     return "prime";
+// }
+// console.log(isprime(13))
+
+//10 Count Digits in a Number
+// function Count(n)
+// {
+//     return String(n).length;
+// }
+// console.log(Count(1285));
+
+//11 Sum of Digits 
+// function sumofdigit(n)
+// {
+//     let sum=0
+//     const numStr=String(n);
+//     for(let i=0;i<numStr.length;i++)
+//     {
+//         sum += parseInt(numStr[i]);
+//     }
+//     return sum;
+// }
+// console.log(sumofdigit(1234))
+
+
+
+// const add =require("./add.js");
+ // import add from "./add.js";
+// console.log(add(10,11));
+// console.log(add(1,5));
+
+
+// const path=require("path");
+
+// console.log("Directory name:",__dirname);
+
+// console.log(path.basename(__dirname+"/myfile.txt"));
+
+// console.log("Full path:",path.resolve(__dirname,"myfile.txt"));
+
+// const moment =require("moment");
+
+// console.log("Date and Time:",moment().format("DD-MM-YYYY HH:MM:SS"));
+
+
+const fs=require("fs");
+
+// fs.writeFile("myfile.txt","Hello mca",(err)=>{
+//     if(err) throw err;
+//     console.log("file saved");
+// })
+
+fs.readFile("myfile.txt","utf8",(err,data)=>{
+    if(err) throw err;
+    console.log("File content:",data);
+})
